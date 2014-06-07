77ff1fee8e7206afc9281ef33bd1ce2fa8a5404c
Harman <har@gmail.com>
2014-06-07 15:21:20 +0530

    Updated path of the key

--- src/main/java/com/rav/insurance/constants/CommonConstants.java
+++ src/main/java/com/rav/insurance/constants/CommonConstants.java
@@ -10,9 +10,9 @@ public class CommonConstants {
 	public static final String ACTIVE = "Y";
 
 	public static final String PUBLIC_RSA_KEY = "publicKey";
-	public static final String PUBLIC_RSA_KEY_PATH = "/WEB-INF/classes/com/fameden/secureKeys/public.key";
+	public static final String PUBLIC_RSA_KEY_PATH = "/WEB-INF/classes/com/rav/secureKeys/public.key";
 
 	public static final String PRIVATE_RSA_KEY = "privateKey";
-	public static final String PRIVATE_RSA_KEY_PATH = "/WEB-INF/classes/com/fameden/secureKeys/private.key";
+	public static final String PRIVATE_RSA_KEY_PATH = "/WEB-INF/classes/com/rav/secureKeys/private.key";
 
 }
